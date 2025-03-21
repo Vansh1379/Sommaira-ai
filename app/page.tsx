5b0a4e3da638881e2d4bd332d2444108644d9294
Vansh1379 <vanshkalra1379@gmail.com>
2025-03-21 13:08:13 +0530

    Added demo section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import BgGradient from "@/components/common/bg-gradient";
 import DemoSection from "@/components/home/demo-section";
 import HeroSection from "@/components/home/hero-section";
+import HowItWorks from "@/components/home/how-it-works";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="flex flex-col ">
         <HeroSection />
         <DemoSection />
+        <HowItWorks />
       </div>
     </div>
   );
--- components/home/demo-section.tsx
+++ components/home/demo-section.tsx
@@ -3,7 +3,7 @@ import { Pizza } from "lucide-react";
 export default function DemoSection() {
   return (
     <section className="relative">
-      <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 m:px-6 lg:px-8">
+      <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 m:px-6 lg:px-8 lg:pt-12">
         <div
           aria-hidden="true"
           className="pointer-events-none absolute inset-x-0 -top-40
@@ -20,12 +20,22 @@ export default function DemoSection() {
             opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72rem]"
           />
         </div>
-        <div>
-          <Pizza className="w-6 h-6 text-rose-600" />
-          <h3>
-            Watch how Sommaire transforms this Next.js course PDF into an
-            easy-to-read summary!
-          </h3>
+        <div className="flex flex-col items-center text-center space-y-4 ">
+          <div className="inline-flex  items-center justify-center p-2 rounded-2xl bg-gray-100/80 backdrop-blur-xs border broder-gray-500/20 mb-4  ">
+            <Pizza className="w-6 h-6 text-rose-600" />
+          </div>
+          <div className="text-center mb-16">
+            <h3 className="font-bold text-3xl max-w-2xl mx-auto px-4 sm:px-6 ">
+              Watch how Sommaire transforms{" "}
+              <span className="bg-linear-to-r from-rose-500 to-rose-700 bg-clip-text text-transparent  ">
+                this Next.js course PDF
+              </span>{" "}
+              into an easy-to-read summary!
+            </h3>
+          </div>
+          <div className="flex justify-center items-center px-2 sm:px-4 lg:px-6">
+            {/** Summary viewer */}
+          </div>
         </div>
       </div>
     </section>
