64d0e32160ff2409f8308f7fb7ae6e2fdca1c368
Vansh1379 <vanshkalra1379@gmail.com>
2025-10-23 00:13:00 +0530

    added zod schema in the uplaod section

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,6 +1,20 @@
 "use client";
 import React from "react";
 import UploadFormInput from "./upload-form-input";
+import { refine, z } from "zod";
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "Invalid File" })
+    .refine(
+      (file) => file.size <= 24 * 1024 * 1024,
+      "File size must be less than 24MB"
+    )
+    .refine(
+      (file) => file.type.startsWith("application/pdf"),
+      "File must be a PDF"
+    ),
+});
 
 export default function UploadForm() {
   const handelSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -8,6 +22,7 @@ export default function UploadForm() {
     console.log("submit");
     const formData = new FormData(e.currentTarget);
     const file = formData.get("file") as File;
+    const result = schema.safeParse(file);
   };
   return (
     <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
